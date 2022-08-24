1bf86e0bd142b1a1c4f9c009c8a8501d10e3c56e
Luis Nava <levacarrillo@gmail.com>
2022-08-24 01:02:10 +0000

    Added pid.yaml into .gitignore added params and filter for battery_reader.py added batt_threshold.yaml added params into minibot.launch

--- catkin_ws/src/battery_reader/scripts/battery_reader.py
+++ catkin_ws/src/battery_reader/scripts/battery_reader.py
@@ -14,7 +14,12 @@ GPIO.setup(24, GPIO.IN)
 GPIO.setup(25, GPIO.IN)
 GPIO.setup(8,  GPIO.IN) 
 
+
+batt_readings = []
 batt_percentage = 0
+
+min_load = rospy.get_param('/min_load', '418')
+max_load = rospy.get_param('/max_load', '528')
 
 class bcolors:
     HEADER = '\033[95m'
@@ -32,10 +37,15 @@ def callback(data):
 
     min_perc = 20#%
     max_perc = 99#%
-    min_analog = 418
-    max_analog = 528
+    min_analog = min_load
+    max_analog = max_load
 
-    batt_percentage = min_perc + (data.data - min_analog) * (max_perc - min_perc) / (max_analog - min_analog)
+    batt_reading = min_perc + (data.data - min_analog) * (max_perc - min_perc) / (max_analog - min_analog)
+    batt_readings.append(batt_reading)
+    
+    if(len(batt_readings) > 10): batt_readings.pop(0)
+
+    batt_percentage = sum(batt_readings) / len(batt_readings)
 
     if(batt_percentage < 0): batt_percentage = 0 
     if(batt_percentage > 99): batt_percentage = 99 
@@ -71,7 +81,8 @@ def battery_reader():
                 else:
                     rospy.set_param('battery_complete', False)
                 if GPIO.input(8):
-                    print(bcolors.WARNING + "BATTERY IS CHARGING" + bcolors.ENDC + " -- Battery at->" + bcolors.BOLD + str(batt_percentage) + "%" + bcolors.ENDC)
+                    if(batt_percentage != 0):
+                        print(bcolors.WARNING + "BATTERY IS CHARGING" + bcolors.ENDC + " -- Battery at->" + bcolors.BOLD + str(batt_percentage) + "%" + bcolors.ENDC)
                     rospy.set_param('battery_charging', True)
                 else:
                     rospy.set_param('battery_charging', False)
